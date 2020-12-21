c2a80bcfe9e4fe879328fe1165512c6fb9014a4b
partik-kaur <parteekkaur3@gmail.com>
2020-12-21 13:20:37 +0530

    today's work- 21-12-2020

--- app.js
+++ app.js
@@ -2,9 +2,38 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     console.log('Hello, Server Side');
-    console.log(req.body);
+    console.log(req.url , req.method, req.headers);
+    //console.log(res);
+    if(req.url == '/')
+    {
+        if (req.method == 'POST')
+        {
+            console.log(req);
+            res.setHeader('Content-Type' , 'text/html');
+            res.write("<html><title>Anything!</title>");
+            res.write("<body><h1>Hello, NodeJs...</h1></body></html>")
+            res.end();
+        }
+        else 
+        {
+        res.setHeader('Content-Type' , 'text/html');
+        res.write("<html><title>Anything!</title>");
+        res.write("<body><h1>Hello, NodeJs...</h1></body></html>")
+        res.end();
+        }
+    }
+    else if(req.url == '/message')
+    {
+        res.setHeader('Content-Type' , 'text/html');
+        res.write("<html><title>Message</title>");
+        res.write("<body><form action = '/' method = 'POST'><input type = 'text' name = 'myinput' placeholder = 'Enter Your Message Here...'>");
+        res.write("<input type = 'submit' value = 'Send Message!'> ");
+        res.write("</form></body>");
+        res.write("</html>");
+        return res.end();
+    }   
 });
 
 server.listen(3000 , () => {
-    console.log('Server Is Running on 3000.');
+    console.log('Server Is Running onn 3000.');
 })
